b21530a8dd7336cde97a5e8cad753d8e2c779daa
Tswap007 <tswap700@gmail.com>
2023-08-22 19:25:09 +0100

    Added the contract now to edit them to my custom features

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    arbitrumGoerli: {
+      url: process.env.ARBITRUM_GOERLI_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    arbitrumGoerli: {
+      url: process.env.MUMBAI_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
